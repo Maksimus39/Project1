fd6852546de3670f48591543b4f9a48f51a652cb
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-12-17 22:24:06 -0800

    project 1 day 2

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -6,6 +6,9 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Storm Viever"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
@@ -26,5 +29,12 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
         cell.textLabel?.text = picture[indexPath.row]
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectImage = picture[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 }
